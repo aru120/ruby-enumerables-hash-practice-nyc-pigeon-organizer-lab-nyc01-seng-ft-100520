e471ee58413caaf896911edcc20324f310f365d9
Kevin Huang <khuang159@gmail.com>
2020-09-18 14:25:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key,value), i|
    
-    value.each do |innerk,innern|
-      k.each do |name|
+    value.each do |inner_k,inner_n|
+      inner_n.each do |name|
      if !i|[name] 
        i[name] = {}
       end
