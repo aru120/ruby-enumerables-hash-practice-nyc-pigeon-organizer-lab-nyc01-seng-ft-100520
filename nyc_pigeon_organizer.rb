554f00dd805fc0828d256a55d8e6cff8b2d865db
Kevin Huang <khuang159@gmail.com>
2020-09-18 14:37:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
    
     value.each do |inner_k,inner_n|
       inner_n.each do |name|
-     if !i|[name] 
+     if !i[name] 
        i[name] = {}
       end
       if !i[name][key]
