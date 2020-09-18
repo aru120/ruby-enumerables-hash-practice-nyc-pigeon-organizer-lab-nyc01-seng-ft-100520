9aaa3e09b587b3003e468f3b0f6a35a38b32d097
Kevin Huang <khuang159@gmail.com>
2020-09-18 14:21:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key,value), i|
+   
+    value.each do |innerk,innern|
+      k.each do |name|
+      binding.pry  
+    end
+      
+  end
+  end
 end
